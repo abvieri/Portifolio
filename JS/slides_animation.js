64c06370db32c0d8cb7f03da14112f40ff41628e
abvieri <vieriabraao@gmail.com>
2024-02-03 14:53:06 -0300

    03/02

--- JS/slides_animation.js
+++ JS/slides_animation.js
@@ -18,24 +18,18 @@ function animation() {
     const carousel_item = document.getElementsByClassName('crl');
     const slide01 = document.getElementById('slide01');
     const slide02 = document.getElementById('slide02');
-    const slide03 = document.getElementById('slide03');
     let arr = Array.from(carousel_item);
-    for (let i = 0; i < arr.length; i++) {
-        const e = arr;
-        if (e[i].classList[3] == "active") {
-            if (e[i].classList[2] == "value_01") {
+
+    arr.forEach(e => {
+        console.log(e);
+        if (e.classList[3] == "active") {
+            if (e.classList[2] == "value_01") {
                 slide02.style.display = "none";
-                slide03.style.display = "none";
                 slide01.style.display = "flex";
-            } else if (e[i].classList[2] == "value_02") {
+            } else if (e.classList[2] == "value_02") {
                 slide01.style.display = "none";
-                slide03.style.display = "none";
                 slide02.style.display = "flex";
-            } else if (e[i].classList[2] == "value_03") {
-                slide01.style.display = "none";
-                slide02.style.display = "none";
-                slide03.style.display = "flex";
             }
         }
-    }
+    });
 }
